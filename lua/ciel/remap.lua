e91ec06a34f1c17bde408845aaf0dcae39a27590
Ciel <ciel@lawrance.co.uk>
2023-03-07 16:05:56 +0000

    Added comments to the remap.lua file and added rebinds to vertically center code after <C-d/u>

--- lua/ciel/remap.lua
+++ lua/ciel/remap.lua
@@ -1,4 +1,12 @@
+-- Sets the leader key to a space
 vim.g.mapleader = " "
+-- Sets short cut for ':Ex' command
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Rebinds j and k to each other
 vim.keymap.set("n", "j", "k")
 vim.keymap.set("n", "k", "j")
+
+-- Adds zz to <C-d/u> shortcuts to center code vertically after move 
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
