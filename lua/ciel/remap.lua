243bca369e7e8331b97febc80629416b03a360a8
Ciel <ciel@lawrance.co.uk>
2023-03-07 18:23:52 +0000

    Commented out sketchy fix for remapping :q to :Q - need to fix later. Added Visual mode to the j and k remapping.

--- lua/ciel/remap.lua
+++ lua/ciel/remap.lua
@@ -3,10 +3,13 @@ vim.g.mapleader = " "
 -- Sets short cut for ':Ex' command
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
--- Rebinds j and k to each other
-vim.keymap.set("n", "j", "k")
-vim.keymap.set("n", "k", "j")
+-- Rebinds j and k to each other - normal and visual
+vim.keymap.set({"n", "v"}, "j", "k")
+vim.keymap.set({"n", "v"}, "k", "j")
 
 -- Adds zz to <C-d/u> shortcuts to center code vertically after move 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Remaps ':q!' to ':Q'
+-- vim.keymap.set("c", "Q", vim.cmd.quit)
